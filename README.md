cea761c587054c5eaf026b51c09a925d12b264c9
Fernando Alves <fernandu.alves@gmail.com>
2022-07-20 09:28:30 -0300

    first commit with all files

---
+++ README.md
@@ -0,0 +1,54 @@
+# Tip calculator app solution
+
+## Table of contents
+
+  - [Overview](#overview)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+  - [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Author](#author)
+
+
+
+## Overview
+
+This Tip Calculator should determine the tip amount and the total check per person based on suggested percentages besides a custom input to write larger amounts.
+
+Users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for interactive elements on the page
+- Calculate the correct tip and total cost of the bill per person
+
+### Screenshot
+
+![](./screenshot_mobile.png)
+
+
+### Links
+
+- Live Site URL:(https://sweet-marigold-272dfa.netlify.app/)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+
+
+### What I learned
+
+I've learned a little bit more about CSS Grid in this project and in the Javascript part I've learned more about event listeners and how to use a event listener for multiple selectors like the buttons in the calculator.
+
+
+## Author
+
+
+- Twitter - (https://www.twitter.com/@fernandu00)
+
---
+++ index.js
@@ -0,0 +1,100 @@
+const check = document.getElementById('check')
+const people = document.getElementById('people')
+const totalPerPerson = document.querySelector('.tip-total')
+const totalWithTips = document.querySelector('.check-total')
+const buttons = document.querySelectorAll('.tip-percentage')
+const customTip = document.querySelector('.custom')
+const resetBtn = document.querySelector('.reset-btn')
+
+let checkValue = 0
+let buttonValue = 0
+let numberPeople = 1
+let tiplPerClient = 0
+let totalWithTipsPerPerson = 0
+let custom = 0
+
+
+// listens to check number
+check.addEventListener('input', (e) => {
+    checkValue = parseFloat(e.target.value)
+    calculateTip()
+})
+
+
+
+// custom Tip event listener
+customTip.addEventListener('input', (e) =>{
+    buttonValue = parseFloat(e.target.value)
+    calculateTip()
+})
+    
+    
+
+// listens to tip percentage   
+buttons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        buttonValue = parseFloat(e.target.value)
+        calculateTip()
+    })
+    
+})
+
+
+
+
+
+// function getTip(){
+//     let buttonValue = parseFloat(buttons.value)
+//     console.log(buttonValue)
+// }
+
+// listens to number of people
+people.addEventListener('input', (e) => {
+    numberPeople = parseFloat(e.target.value)
+    calculateTip()
+  
+
+})
+
+
+function calculateTip(){
+    // console.log(`checkValue ${checkValue},buttonValue ${buttonValue},numberPeople ${numberPeople}`)
+    tipPerClient = (checkValue * buttonValue) / numberPeople
+    totalWithTipsPerPerson = (checkValue + (checkValue * buttonValue)) /numberPeople
+    if(Number.isNaN(tipPerClient) || Number.isNaN(totalWithTipsPerPerson)){
+        tipPerClient = 0.0
+        totalWithTipsPerPerson = 0.0
+    }
+
+    totalPerPerson.innerHTML = `$ ${tipPerClient.toFixed(2)}`
+    totalWithTips.innerHTML = `$ ${totalWithTipsPerPerson.toFixed(2)}`
+    if(totalWithTipsPerPerson.toFixed(2) > 1000000 && tipPerClient.toFixed(2) > 1000000){
+        totalWithTips.style.fontSize = "1.2rem"
+        totalPerPerson.style.fontSize = "1.2rem"
+    }else{
+        totalWithTips.style.fontSize = "1.75rem"
+        totalPerPerson.style.fontSize = "1.75rem"
+    }
+
+
+} 
+
+resetBtn.addEventListener('click', (e) => {
+    totalPerPerson.innerHTML = ""
+    totalWithTips.innerHTML = ""
+    customTip.value = ""
+    people.value = ""
+    check.value = ""
+})
+
+// calculateTip()
+// calculate tip
+
+// function calculateTipPerPerson(){
+//     total = parseFloat((checkTotal * tipPercentage) /numberPeople)
+    
+    
+// }
+
+
+// calculateTipPerPerson(checkTotal,tipPercentage,numberPeople)
